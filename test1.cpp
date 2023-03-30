996fb4ce7b5321c37166b4998d40ef79e272fcf6
empiretylh <thuralinhtut3@gmail.com>
2023-03-30 11:54:24 +0630

    Guide

---
+++ test1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+
+    std::string text;
+    std::cout << "Type Something : ";
+    std::cin >> text;
+    std::cout << text << std::endl;
+
+    return 0;
+
+}
